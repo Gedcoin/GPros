f0cc9e40eb296935fcc55644a5a3a11c7f9709ca
Gedcoin <gedcoin@gmail.com>
2020-05-14 21:11:05 +0200

    Initial Commit

---
+++ JokeAPI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JokeAPI
+//
+//  Created by Gedcoin Maboya on 2020/05/14.
+//  Copyright © 2020 LTI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
